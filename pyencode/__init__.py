efde2f421599dfea40fdb084cfde405326650830
Konstantin Tretyakov <kt@ut.ee>
2014-06-12 14:32:52 +0300

    Initial package structure

---
+++ pyencode/__init__.py
@@ -0,0 +1,22 @@
+'''
+PyENCODE: Main module
+
+Copyright 2014, Konstantin Tretyakov
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'pyencode' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+PyENCODE: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2014, Konstantin Tretyakov
+Licensed under MIT
+'''
